2eac75cedcc16b60725f8dfc87cc77c46cc4127c
Joffre Gavidia <joffregh3002@gmail.com>
2025-04-02 09:42:25 -0500

    finalize header and footer, add initial auto-carousel version

---
+++ src/components/shared/HorizontalScroll.jsx
@@ -0,0 +1,32 @@
+import siteData from "../../site.json";
+import "./HorizontalScroll.css";
+
+const HorizontalScroll = () => {
+  return (
+    <div className="horizontal-scroll-wrapper">
+      <div className="horizontal-scroll-content">
+        {siteData.support.images.map((image, index) => (
+          <div key={index} className="scroll-item">
+            <img
+              src={image.src}
+              alt={image.alt || `Logo ${index + 1}`}
+              loading="lazy"
+            />
+          </div>
+        ))}
+        {/* Duplicamos las imágenes para el efecto infinito */}
+        {siteData.support.images.map((image, index) => (
+          <div key={`clone-${index}`} className="scroll-item">
+            <img
+              src={image.src}
+              alt={image.alt || `Logo ${index + 1}`}
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
