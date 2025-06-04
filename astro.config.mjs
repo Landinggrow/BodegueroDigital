3ea622325853b1416a598ff7c1ef141df686dc56
Joffre Gavidia <joffregh3002@gmail.com>
2025-06-04 11:14:48 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-// https://astro.build/config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  output: "static",
+  adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react(), mdx()],
+  server: {
+    host: true, // Permite accesos desde cualquier IP
+    port: 4321,
+    allowedHosts: [
+      "localhost",
+      // 'webnuberest.proudgrass-e138b78f.brazilsouth.azurecontainerapps.io'
+    ], // Hosts permitidos para evitar bloqueos en producción
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Evita problemas de CORS
+    },
+  },
 });
